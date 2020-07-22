71c4b1a1209640ef6fb4c6385e5e44e9623af81a
Gras64 <gras64@web.de>
2020-07-22 20:46:27 +0000

    fix raspberry display

--- __init__.py
+++ __init__.py
@@ -12,23 +12,26 @@ class Standbymonitor(MycroftSkill):
         self.settings["monitor"] = self.settings.get('monitor', 1)
         self.settings["timer"] = self.settings.get('timer', 60)
         if self.settings["auto"]:
-            self.add_event('recognizer_loop:audio_output_end',
+            self.add_event('enclosure.mouth.reset',
                         self.handle_standby)
             self.add_event('recognizer_loop:wakeword',
                         self.ex_wakeup)
             self.ex_wakeup()
+        self.log.info("set auto mode to: "+str(self.settings["auto"]))
 
     def handle_standby(self):
         time = self.settings["timer"]
-        self.schedule_event(self.ex_standby, 30, name='standby')
+        self.log.info("add standby timer")
+        self.schedule_event(self.ex_standby, time, name='standby')
 
     def ex_standby(self):
+        self.log.info("handle standby")
         if int(self.settings["monitor"]) == 1:
             self.log.info(self.settings["monitor"])
             subprocess.call("/usr/bin/xset dpms force off",
                                     preexec_fn=os.setsid, shell=True)
         elif int(self.settings["monitor"]) == 2:
-            subprocess.call("sudo cp off /sys/class/backlight/rpi_backlight/bl_power",
+            subprocess.call("echo 1 > /sys/class/backlight/rpi_backlight/bl_power",
                                     preexec_fn=os.setsid, shell=True)
         elif int(self.settings["monitor"]) == 3:
             subprocess.call("usr/sbin/i2cset  -y $BUS 0x1b 0x16 0x00 0x00 0x00 0x00 i",
@@ -45,11 +48,14 @@ class Standbymonitor(MycroftSkill):
         
 
     def ex_wakeup(self):
+        self.log.info("handle wakeup")
         if int(self.settings["monitor"]) == 1:
             subprocess.call("/usr/bin/xset dpms force on",
                                     preexec_fn=os.setsid, shell=True)
         elif int(self.settings["monitor"]) == 2:
-            subprocess.call("sudo cp on /sys/class/backlight/rpi_backlight/bl_power",
+            subprocess.call("echo 0 > /sys/class/backlight/rpi_backlight/bl_power",
+                                    preexec_fn=os.setsid, shell=True)
+            subprocess.call("echo '255' > /sys/class/backlight/rpi_backlight/brightness",
                                     preexec_fn=os.setsid, shell=True)
         elif int(self.settings["monitor"]) == 3:
             subprocess.call("/usr/sbin/i2cset -y $BUS 0x1b 0x16 0x00 0x00 0x00 0x07 i",
