3d9716d79a2df625b7c787c48fdfd778c97b426f
Andreas Reinle <gras64@web.de>
2020-07-26 12:33:53 +0200

    fix intent fail

--- __init__.py
+++ __init__.py
@@ -14,7 +14,7 @@ class Standbymonitor(MycroftSkill):
         if self.settings["auto"]:
             self.add_event('enclosure.mouth.reset',
                         self.handle_standby)
-            self.add_event('complete_intent_failure',
+            self.add_event('recognizer_loop:speech.recognition.unknown',
                         self.ex_standby)
             self.add_event('recognizer_loop:wakeword',
                         self.ex_wakeup)
